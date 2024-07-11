19298956e754d1b470db4daa805408004bea37b3
blaisep <blaise@gmail.com>
2024-07-11 14:55:38 -0700

    Add stubs for more tests of sad paths
    Ch 12 exercise Bonus; thinking of more failure scenarios: files with no data, wrong data type (eg jpeg, text)
    
    Co-authored by: Jeff Jacobson <jeffjacobsonhimself@gmail.com>

--- code/exercises/ch12/test/test_output.py
+++ code/exercises/ch12/test/test_output.py
@@ -2,7 +2,8 @@ import sums
 import pytest
 
 def test_sad_path(capsys):
-    with pytest.raises(SystemExit):
+    """Capsys never gets an argument because it can't access the system args :("""
+    with pytest.raises(Exception):
         sums.main()
     # output = capsys.readouterr().out
     # assert output == '200.00\n'
--- code/exercises/ch12/test/test_sums.py
+++ code/exercises/ch12/test/test_sums.py
@@ -1,7 +1,10 @@
 from subprocess import run
 import pytest
 
+
 def test_happy_path():
+    """Happy path"""
+
     result = run(['python',
                       'src/sums.py',
                       'src/data.txt'],
@@ -10,8 +13,21 @@ def test_happy_path():
     assert output == '200.00\n'
 
 def test_sad_path():
+    """No data file provided"""
+
     result = run(['python',
                   'src/sums.py'],
                  capture_output=True, text=True)
     assert result.returncode == 1
     assert result.stderr.endswith('Usage: src/sums.py <datafile>\n')
+
+def test_bad_file():
+    """A file with no data should raise an exception."""
+
+    pass
+
+def test_bad_content():
+    """A file with bad content should raise an exception."""
+
+    pass
+# is there a pytesty way to "pass"
